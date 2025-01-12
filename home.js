48c126604ce14e79eaea251c822b66c2e91d861c
jason-23-coder <pangpau@graduate.utm.my>
2025-01-12 11:40:33 +0800

    Update home.js

--- home.js
+++ home.js
@@ -78,15 +78,15 @@ async function loadUserBorrowDetails(userEmail) {
         dateCell.textContent = borrowDate.toLocaleDateString();
         row.appendChild(dateCell);
 
-        //Expired Date
-        const expireddateCell = document.createElement("td");
-        if (borrow.expireddate && borrow.expireddate.seconds) {
-          const expiredDate = new Date(application.expireddate.seconds * 1000); // Convert Firestore timestamp to JS Date
-          expireddateCell.textContent = expiredDate.toLocaleDateString(); // Set the formatted date text
-        } else {
-          expireddateCell.textContent = "N/A"; // Set a default value if expireddate is empty
-        }
-        row.appendChild(expireddateCell);
+        // //Expired Date
+        // const expireddateCell = document.createElement("td");
+        // if (borrow.expireddate && borrow.expireddate.seconds) {
+        //   const expiredDate = new Date(application.expireddate.seconds * 1000); // Convert Firestore timestamp to JS Date
+        //   expireddateCell.textContent = expiredDate.toLocaleDateString(); // Set the formatted date text
+        // } else {
+        //   expireddateCell.textContent = "N/A"; // Set a default value if expireddate is empty
+        // }
+        // row.appendChild(expireddateCell);
 
         // Checkbox to select for return
         const returnCell = document.createElement("td");
